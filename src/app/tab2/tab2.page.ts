00d969481fc6b1e847c0232fe32bbd19217bc011
Ana Laura <101823261+Anazz24@users.noreply.github.com>
2023-03-19 22:11:04 -0300

    19-03

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -7,12 +7,53 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  carta=1;
+  carta=0;
   time1=0;
   time2=0;
   partida1=0
   partida2=0
 
   constructor() {}
+
+  alterarValorValendo(valor: number){
+    this.carta = valor;
+  }
+
+  adicionarPontuacaoTime1(){
+    this.time1 += this.carta;
+    this.carta = 1;
+    if(this.time1 >=12){
+      this.partida1 += 1;
+      this.time1=0;
+      this.time2=0;
+    }
+  }
+  adicionarPontuacaoTime2(){
+    this.time2 += this.carta;
+    this.carta = 1;
+    if(this.time2 >=12){
+      this.partida2 += 1;
+      this.time1=0;
+      this.time2=0;
+    }
+  }
+
+  removerPontuacaoTime1(){
+    this.time1 -= this.carta;
+    this.carta = 1;
+  }
+
+  removerPontuacaoTime2(){
+    this.time2 -= this.carta;
+    this.carta = 1;
+  }
+
+  LimparVariaveisDoZero(){
+    this.time1 = 0;
+    this.time2 = 0;
+    this.carta = 0;
+    this.partida1=0;
+    this.partida2=0;
+  }
 
 }
